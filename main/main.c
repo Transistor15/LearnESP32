018a3322ac204a79ab484f8686ba872a1c4ada2d
Transistor15 <cephasmorgans@gmail.com>
2023-02-23 10:21:49 -0800

    simple delay using portTick_PERIOD_MS)

--- main/main.c
+++ main/main.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#define TAG "DELAY"
 void app_main(void)
 {
-    esp_log_level_set("LOG",ESP_LOG_INFO);
-ESP_LOGE("LOG","This is an error");
-ESP_LOGW("LOG", "Thus is a warning");
-ESP_LOGI("LOG","This is info log");
-ESP_LOGD("LOG","This is an debug");
-ESP_LOGV("LOG","This is an verbose");
 int i=0;
-ESP_LOGE("TAG","This is an error %d",i++);
-ESP_LOGW("TAG", "Thus is a warning %d",i--);
-ESP_LOGI("TAgG","This is info log %d",i++);
-ESP_LOGD("TAG","This is an debug%d",i--);
-ESP_LOGV("TAG","This is an verbose%d",i++);
+while(1)
+{
+    vTaskDelay(1000 /portTICK_PERIOD_MS);
+    ESP_LOGI(TAG, "in loop%d",i++);
+}
 }
